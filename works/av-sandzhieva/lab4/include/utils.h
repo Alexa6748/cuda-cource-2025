cfe39d607bd47e1c8aa84cc91290b47015969319
Александра Санджиева <73139557+Alexa6748@users.noreply.github.com>
2026-02-16 14:58:03 +0300

    Add utils.h

---
+++ works/av-sandzhieva/lab4/include/utils.h
@@ -0,0 +1,24 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
+
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <cstdio>
+
+#define CUDA_CHECK(call) \
+    do { \
+        cudaError_t err = (call); \
+        if (err != cudaSuccess) { \
+            fprintf(stderr, "CUDA error in %s:%d — %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (0)
+
+#endif // UTILS_H
