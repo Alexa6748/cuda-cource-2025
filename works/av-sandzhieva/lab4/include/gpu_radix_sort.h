a7bf519f314e1b4bac135c5984673134e81f41b8
Александра Санджиева <73139557+Alexa6748@users.noreply.github.com>
2026-02-16 15:05:43 +0300

    Create gpu_radix_sort.h

---
+++ works/av-sandzhieva/lab4/include/gpu_radix_sort.h
@@ -0,0 +1,17 @@
+#ifndef GPU_RADIX_SORT_H
+#define GPU_RADIX_SORT_H
+
+#include <cstddef>
+
+#define BLOCK_SIZE         1024
+#define WARP_SIZE          32
+#define WARPS_PER_BLOCK    (BLOCK_SIZE / WARP_SIZE)
+#define RADIX              256
+#define BITS_PER_PASS      8
+#define LOG_BANKS          5
+#define OFFSET(idx)        ((idx) >> LOG_BANKS)
+
+template<typename T>
+void gpu_radix_sort(T* d_out, T* d_in, size_t n);
+
+#endif // GPU_RADIX_SORT_H
